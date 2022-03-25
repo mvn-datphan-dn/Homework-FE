7217ae65523b2e9d2288d344216baf5b561fa07a
mvn-datphan-dn <dat.phan@monstar-lab.com>
2022-03-25 08:57:17 +0700

    bt javascript day3

---
+++ jsday3/someApp.js
@@ -0,0 +1,4 @@
+//someApp.js
+import { Utils } from "./lib/utils";
+var math = Utils;
+console.log("2π = " + math.sum(math.pi, math.pi)); 
---
+++ jsday3/test.js
@@ -0,0 +1,8 @@
+export {
+    greetPerson
+};
+ 
+// exporting a function
+function greetPerson(name) {
+    return `Hello ${name}`;
+}
